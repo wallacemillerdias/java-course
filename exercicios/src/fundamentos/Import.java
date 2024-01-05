3b5640e33e49794463652d9a94e4fa3326036623
wallacemiller <wallacemillerdias@gmail.com>
2024-01-05 17:11:49 -0300

    :sparkles: Import

---
+++ exercicios/src/fundamentos/Import.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class Import {
+	
+	public static void main(String[] args) {
+		
+		java.lang.String b = "Boa tarde";
+		java.lang.System.out.println(b);
+		
+		Date d = new Date();
+		System.out.println(d);
+	}
+
+}
