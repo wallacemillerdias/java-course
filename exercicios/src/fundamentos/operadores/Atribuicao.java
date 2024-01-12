e2410f236815a0e7865c9b8468f3d94f2065474a
wallacemiller <wallacemillerdias@gmail.com>
2024-01-12 09:56:28 -0300

    :sparkles: Operadores de Atribuição

--- exercicios/src/fundamentos/operadores/Atribuicao.java
+++ exercicios/src/fundamentos/operadores/Atribuicao.java
@@ -4,7 +4,18 @@ public class Atribuicao {
 
 	public static void main(String[] args) {
 		
-		var c = c++;
+		int a = 3;
+		int b = a;
+		int c = a + b;
+		
+		c += b ;
+		c -= b ;
+		c *= b ;
+		c /= b ;
+		
+		System.out.println(c);
+		
+		c %= 2;
 		System.out.println(c);
 	}
 }
